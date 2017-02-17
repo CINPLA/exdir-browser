afa1deb4f67dde49437ec26c30db621490875178
Svenn-Arne Dragly <s@dragly.com>
2017-02-17 16:13:52 +0100

    Update entry_points

--- setup.py
+++ setup.py
@@ -6,14 +6,11 @@ from setuptools import setup, find_packages
 
 long_description = open("README.md").read()
 
-entry_points = None
-
 install_requires = []
 
 setup(
     name="Exdir Browser",
     packages=find_packages(),
-    entry_points=entry_points,
     include_package_data=True,
     entry_points='''
         [console_scripts]
