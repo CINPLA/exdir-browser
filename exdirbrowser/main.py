b06d0fbaa7c122b5093d878d0e4a48ed8def5970
Svenn-Arne Dragly <s@dragly.com>
2017-02-18 12:41:50 +0100

    Restructure libGL loading

--- exdirbrowser/main.py
+++ exdirbrowser/main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-import ctypes
-from ctypes.util import find_library
 import sys
 
-from sys import platform
-if platform == "linux" or platform == "linux2":
+if sys.platform == "linux" or sys.platform == "linux2":
     # TODO remove this OpenGL fix when PyQt doesn't require OpenGL to be loaded first. 
     # NOTE This must be placed before any other imports!
+    import ctypes
+    from ctypes.util import find_library
     libGL = find_library("GL")
     ctypes.CDLL(libGL, ctypes.RTLD_GLOBAL)
 
